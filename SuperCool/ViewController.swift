2f1290fd3afef57669a86556ea5037ea5243bcf7
Michael Demissie <Mikegd7@Michaels-MBP.fios-router.home>
2016-07-11 13:23:09 -0400

    added files for supercool

--- SuperCool/ViewController.swift
+++ SuperCool/ViewController.swift
@@ -7,9 +7,25 @@
 //
 
 import UIKit
-
 class ViewController: UIViewController {
 
+    var num:Int = 0
+    
+    @IBAction func tapped(sender: AnyObject) {
+        self.view.backgroundColor = getRandomColor()
+        sender.setTitleColor(getRandomColor(), forState: .Normal)
+        num += 1
+        if num%2 == 0 && num%3 == 0 {
+            sender.setTitle("Easy! I have feelings too.", forState: .Normal)
+        }
+        else if num%2 != 0 && num%3 == 0 {
+            sender.setTitle("Could you slow down a bit?", forState: .Normal)
+        }
+        else {
+            sender.setTitle("Tap Me!", forState: .Normal)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +35,15 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func getRandomColor() -> UIColor{
+        let randomRed:CGFloat = CGFloat(drand48())
+        
+        let randomGreen:CGFloat = CGFloat(drand48())
+        
+        let randomBlue:CGFloat = CGFloat(drand48())
+        
+        return UIColor(red: randomRed, green: randomGreen, blue: randomBlue, alpha: 1.0)
+    }
 }
 
